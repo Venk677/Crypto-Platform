efaa7f81ebc7e5656f93b543d3340390acdfbbdf
Venk333 <venkatesh.vk677@gmail.com>
2022-01-05 09:27:39 +0530

    skeleton of the app created

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import{Button, Menu, Typography, Avatar} from 'antd';
+import{ Link } from 'react-router-dom';
+import{ HomeOutlined, MenuFoldOutlined, FundOutlined,BulbOutlined, MoneyCollectOutlined} from '@ant-design/icons'; 
+import { icons } from 'antd/lib/image/PreviewGroup';
+import icon from '../images/cryptocurrency.png'
+const Navbar = () => {
+    return (
+        <div className='="nav-container'>
+            <div className='="logo-container'>
+                <Avatar src ={icon} size="large"/>
+                <Typography.Title level={2} className='logo'>
+                    <Link to="/"> Enghe Crypto </Link>
+                </Typography.Title>
+                {/* <Button className='menu-control-container'>
+
+                </Button> */}
+            </div>
+            
+        </div>
+    )
+}
+
+export default Navbar
